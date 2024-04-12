940e8918bcd7a53392a06e08f47c48f94d66d4fc
LeticiaRompava <leticia.rompava@escola.pr.gov.br>
2024-04-12 12:11:10 -0300

    subir aula

--- app.py
+++ app.py
@@ -1,44 +1,61 @@
-#1 10/04 importar lib os(operationas system)
-import os
+import os 
 
-#2 declarando a função finalizar_app
+restaurante=[] 
+
 def finalizar_app():
     os.system('cls')
-    print('Finalizando o app\n')
+    print ('Finalizando app\n')
+
+def chamar_name_do_app():
+     print ('Restaurante Expresso\n') 
+
+def opcao_invalida():
+    print('Opção invalida\n')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main() 
+
+def exibir_opcoes():
+    print('1-Cadastrar um restaurante')
+    print('2-Listar restaurantes')
+    print('3-Ativar um restaurante')
+    print('4-Sair do programa') 
 
 
-
-def chamar_nome_do_app():
- print('Restaurante Expresso\n')
-
-
-def escolher_opcoes():
- print ('1-Cadastrar um restaurantes')
- print ('2-Listar restaurantes')
- print ('3-Ativar um restaurante')
- print ('4-Sair do programa\n')
-
-
-opcao_digitada=int(input('Escolha uma opção: '))
-print('Você selecionou a opção:', opcao_digitada,'\n')
-
-if(opcao_digitada==1):
-    print('Você escolheu cadastrar um restaurante \n')
-elif(opcao_digitada==2):
-    print('Você escolheu listar os restaurante do app \n')
-elif(opcao_digitada==3):
-    print('Você escolheu ativar um restaurante \n')
-else:
-    print('Você escolheu sair do programa\n')
-    #3 chamada a função finalizar_app
-    finalizar_app()
-
-    #5 escrever a função main
-    def main():
-       #6 chamar o nome do app
-    
-
-    #4 criando a entrega através da variável main
-    if __name__=='__Main__': # type: ignore
+def cadastrar_novo_restaurante():
+     os.system('cls')
+     nome_do_restaurante=input('Digite o nome do novo restaurante: \n')
+     restaurante.append(nome_do_restaurante)
+     print(f'O restaurante {nome_do_restaurante}, foi cadastrado com sucesso\n')
+     input("Digite uma tecla para voltar ao menu principal: \n")
      main()
-     
+
+def escolher_opcoes(): 
+    try:
+        opcao_digitada=(int(input('Escolha uma opção: ')))
+        print('Você selecionou a opção: ',opcao_digitada,'\n')
+        if(opcao_digitada==1):
+            print('Você escolheu cadastrar um restaurante\n')
+            cadastrar_novo_restaurante()
+        elif (opcao_digitada==2):
+            print('Você escolheu listar os restaurantes do app\n')
+        elif (opcao_digitada==3):
+            print('Você escolheu ativar um restaurante\n')
+        elif (opcao_digitada==4):
+            print('Você escolheu sair do programa\n')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida() 
+
+def main():
+    os.system('cls') 
+
+    chamar_name_do_app() 
+
+    exibir_opcoes() 
+
+    escolher_opcoes() 
+
+if(__name__=='__main__'):
+    main()
